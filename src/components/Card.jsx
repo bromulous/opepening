98a6b4dc097a3cfa1c9d94c2bb318cf568ece990
Julian Prochaska <jmprochaska@gmail.com>
2023-06-06 13:52:09 -0700

    remove card back text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -47,7 +47,6 @@ const Card = forwardRef(
                     >
                         <img
                             src={backImage}
-                            alt="Card Back"
                             style={{
                                 width: "100%",
                                 height: "100%",
