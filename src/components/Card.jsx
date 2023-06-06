260f0f6b7da3c1f5edb5f67cf0666b34f2c5b4e6
Julian Prochaska <jmprochaska@gmail.com>
2023-06-05 18:32:47 -0700

    Removed opensea tag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -29,28 +29,28 @@ const Card = forwardRef(({ frontImage, backImage, isFlipped, onClick, canFlip, c
                     style={{ width: "100%", height: "100%", objectFit: "contain" }}
                 />
             </div>
-            <div
-                onClick={handleClick}
-                style={{
-                    width: "2in",
-                    height: "2.8in",
-                    borderRadius: "15px",
-                    overflow: "hidden",
-                    cursor: "pointer",
-                    backgroundColor: "black",
-                    display: "flex",
-                    flexDirection: "column",
-                }}
-            >
-                <img
-                    src={frontImage}
-                    alt="Card Front"
-                    style={{ width: "100%", height: "2.5in", objectFit: "contain" }}
-                />
-                <a href={openSeaLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white", textAlign: "center", alignSelf: "center", flexGrow: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    OpenSea
-                </a>
-            </div>
+            <a href={openSeaLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", display: "block" }}>
+                <div
+                    onClick={handleClick}
+                    style={{
+                        width: "2in",
+                        height: "2.8in",
+                        borderRadius: "15px",
+                        overflow: "hidden",
+                        cursor: "pointer",
+                        backgroundColor: "black",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    <img
+                        src={frontImage}
+                        alt="Card Front"
+                        style={{ width: "100%", height: "2.5in", objectFit: "contain" }}
+                    />
+                </div>
+            </a>
         </ReactCardFlip>
     );
 });
